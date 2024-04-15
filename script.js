545224357c55dbd5f277d3a162ce48ba0146a895
Chol <solertorres@gmail.com>
2024-04-15 13:41:34 -0600

    collectEmployees function with working prompts

--- script.js
+++ script.js
@@ -4,6 +4,36 @@ const addEmployeesBtn = document.querySelector('#add-employees-btn');
 // Collect employee data
 const collectEmployees = function() {
   // TODO: Get user input to create and return an array of employee objects
+  let keepAsking=true
+  const employees = [];
+  while (keepAsking){
+    // prompt for first name, store it
+    let employee={};
+    const firstNamePrompt = prompt("Please enter employee's first name");
+    //if (firstName) {employee.fname=firstName;}
+    //prompt for last name, store it
+    const lastNamePrompt = prompt("Please enter employee's last name");
+    //if (lastName) {employee.lname=lastName;}
+    //prompt for salary, store it
+    const salaryPrompt = prompt("Please enter employee's salary");
+    //if (salary) {employee.sal=salary}
+    //ask if i wanna add a new employee
+    if (firstNamePrompt && lastNamePrompt && salaryPrompt) {
+      employee.firstName=firstNamePrompt;
+      employee.lastName=lastNamePrompt;
+      if(isNaN(salaryPrompt)){
+        employee.salary=0;
+      } else employee.salary=Number(salaryPrompt)
+      
+    }
+    //if yes, repeat
+    employees.push(employee)
+    let continueAsking = confirm ("Do you want to add a new employee?")
+    if (!continueAsking) return employees
+
+  }
+
+  //else display employees alphabetically by last name
 }
 
 // Display the average salary
